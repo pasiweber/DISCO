6830eeebaa8b373feb30fba6180719160507c35b
l.krieger <l.krieger@fz-juelich.de>
2024-07-18 11:36:02 +0200

    update DSI to be offical author version

--- src/Evaluation/DSI/dsi.py
+++ src/Evaluation/DSI/dsi.py
@@ -1,33 +1,38 @@
 import numpy as np
+import scipy.spatial.distance as distance
 from scipy.stats import ks_2samp
-from sklearn.metrics import euclidean_distances
+
+# Official implementation from https://github.com/ShuyueG/CVI_using_DSI/blob/main/cluster_DSI_example.py
+def dists(data, dist_func=distance.euclidean):  # compute ICD
+    num = data.shape[0]
+    data = data.reshape((num, -1))
+    dist = []
+    for i in range(0, num - 1):
+        for j in range(i + 1, num):
+            dist.append(dist_func(data[i], data[j]))
+    return np.array(dist)
 
 
-def dsi_score(data, labels):
-    # what kind of distance measure?
-    dist = euclidean_distances(data, data)
+def dist_btw(a, b, dist_func=distance.euclidean):  # compute BCD
+    a = a.reshape((a.shape[0], -1))
+    b = b.reshape((b.shape[0], -1))
+    dist = []
+    for i in range(a.shape[0]):
+        for j in range(b.shape[0]):
+            dist.append(dist_func(a[i], b[j]))
+    return np.array(dist)
 
-    dist = np.array(dist)
 
-    # calculate KS similarity between ICD and BCD for every class
-    KS_list = []
+def dsi_score(data, labels):  # KS test on ICD and BCD
+    classes = np.unique(labels)
+    SUM = 0
+    for c in classes:
+        pos = data[np.squeeze(labels == c)]
+        neg = data[np.squeeze(labels != c)]
 
-    for i in np.unique(labels):
-        ind_i = np.where(labels == i)[0]
-
-        # ICD set: intra-class distances
-        ICD_i = dist[np.ix_(ind_i, ind_i)]
-        elements_ICD = np.triu_indices(len(ind_i), k=1)
-        ICD_vector = ICD_i[elements_ICD]
-
-        # BCD set: between-class distances
-        BCD_vector = dist[np.ix_(ind_i, np.setdiff1d(np.arange(len(labels)), ind_i))].flatten()
-
-        # calculate KS statistic
-        KS_i = ks_2samp(ICD_vector, BCD_vector).statistic
-        KS_list.append(KS_i)
-
-    # average KS similarity values
-    DSI = np.mean(KS_list)
-
-    return DSI
+        dist_pos = dists(pos)
+        distbtw = dist_btw(pos, neg)
+        D, _ = ks_2samp(dist_pos, distbtw)  # KS test
+        SUM += D
+    SUM = SUM / classes.shape[0]  # normed: b/c ks_2samp ranges [0,1]
+    return SUM
