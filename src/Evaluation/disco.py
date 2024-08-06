b04876809627fddf7d7feac47075232d00505a76
Pascal Weber <pascal.weber@univie.ac.at>
2024-08-06 12:12:14 +0000

    disco doku progress

--- src/Evaluation/disco.py
+++ src/Evaluation/disco.py
@@ -1,33 +1,34 @@
 import numpy as np
-import functools
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics.pairwise import pairwise_distances_chunked
 from sklearn.neighbors import KDTree
-from scipy.sparse import issparse
 from sklearn.metrics import silhouette_samples
-
 from src.Evaluation.dcdistances.dctree import DCTree
 
 
 def disco_score(X: np.ndarray, labels: np.ndarray, min_points: int = 5):
-    """Compute the mean Silhouette Coefficient of all samples.
+    """Compute the mean DISCO score of all samples.
 
-    The Silhouette Coefficient is calculated using the mean intra-cluster
-    distance (``a``) and the mean nearest-cluster distance (``b``) for each
-    sample.  The Silhouette Coefficient for a sample is ``(b - a) / max(a,
-    b)``.  To clarify, ``b`` is the distance between a sample and the nearest
+    The DISCO score is calculated using the mean intra-cluster on the
+    dc-distance (``a``) and the mean nearest-cluster on the dc-distance (``b``)
+    for each non noise sample.  The DISCO score for a non noise sample is 
+    ``(b - a) / max(a, b)``.
+    To clarify, ``b`` is the dc-distance between a non noise sample and the nearest
     cluster that the sample is not a part of.
-    Note that Silhouette Coefficient is only defined if number of labels
-    is ``2 <= n_labels <= n_samples - 1``.
+    ``-1`` in labels are considered as noise and their DISCO score is calculated 
+    by the minimum of the two different measures ``p_sparse`` and ``p_far``. 
+    ``p_sparse`` measures how well the noise sample is within a sparse region.
+    ``p_far`` measure how well the noise is remote to non noise samples.
+    Note that DISCO score is defined for all possible number of labels
+    ``1 <= n_labels <= n_samples``.
+    Except for ``-1`` every other value is considered as cluster label.
 
-    This function returns the mean Silhouette Coefficient over all samples.
-    To obtain the values for each sample, use :func:`silhouette_samples`.
+    This function returns the mean DISCO score over all samples.
+    To obtain the values for each sample, use :func:`disco_samples`.
 
     The best value is 1 and the worst value is -1. Values near 0 indicate
     overlapping clusters. Negative values generally indicate that a sample has
     been assigned to the wrong cluster, as a different cluster is more similar.
 
-    Read more in the :ref:`User Guide <silhouette_coefficient>`.
+    Read more in the :ref:`User Guide <disco_score>`.
 
     Parameters
     ----------
@@ -38,79 +39,48 @@ def disco_score(X: np.ndarray, labels: np.ndarray, min_points: int = 5):
     labels : array-like of shape (n_samples,)
         Predicted labels for each sample.
 
-    metric : str or callable, default='euclidean'
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by :func:`~sklearn.metrics.pairwise_distances`. If ``X`` is
-        the distance array itself, use ``metric="precomputed"``.
-
-    sample_size : int, default=None
-        The size of the sample to use when computing the Silhouette Coefficient
-        on a random subset of the data.
-        If ``sample_size is None``, no sampling is used.
-
-    random_state : int, RandomState instance or None, default=None
-        Determines random number generation for selecting a subset of samples.
-        Used when ``sample_size is not None``.
-        Pass an int for reproducible results across multiple function calls.
-        See :term:`Glossary <random_state>`.
-
-    **kwds : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a scipy.spatial.distance metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-
     Returns
     -------
-    silhouette : float
-        Mean Silhouette Coefficient for all samples.
+    disco : float
+        Mean DISCO score for all samples.
 
     References
     ----------
 
-    .. [1] `Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
-       Interpretation and Validation of Cluster Analysis". Computational
-       and Applied Mathematics 20: 53-65.
-       <https://www.sciencedirect.com/science/article/pii/0377042787901257>`_
+    .. [1] `anonymous`_
 
-    .. [2] `Wikipedia entry on the Silhouette Coefficient
-           <https://en.wikipedia.org/wiki/Silhouette_(clustering)>`_
 
     Examples
     --------
-    >>> from sklearn.datasets import make_blobs
-    >>> from sklearn.cluster import KMeans
-    >>> from sklearn.metrics import silhouette_score
-    >>> X, y = make_blobs(random_state=42)
-    >>> kmeans = KMeans(n_clusters=2, random_state=42)
-    >>> silhouette_score(X, kmeans.fit_predict(X))
-    0.49...
+    >>> from sklearn.datasets import make_moons
+    >>> from sklearn.cluster import HDBSCAN
+    >>> from disco import disco_score
+    >>> X, y = make_moons(random_state=42)
+    >>> hdbscan = HDBSCAN()
+    >>> disco_score(X, hdbscan.fit_predict(X))
+    0.71...
     """
 
     return np.mean(disco_samples(X, labels, min_points))
 
 
-def disco_samples(
-    X: np.ndarray,
-    labels: np.ndarray,
-    min_points: int = 5,
-) -> np.ndarray:
-    """Compute the Silhouette Coefficient for each sample.
+def disco_samples(X: np.ndarray, labels: np.ndarray, min_points: int = 5) -> np.ndarray:
+    """Compute the DISCO score for each sample.
 
-    The Silhouette Coefficient is a measure of how well samples are clustered
+    The DISCO score is a measure of how well samples are clustered
     with samples that are similar to themselves. Clustering models with a high
-    Silhouette Coefficient are said to be dense, where samples in the same
+    DISCO score are said to be dense, where samples in the same
     cluster are similar to each other, and well separated, where samples in
     different clusters are not very similar to each other.
 
-    The Silhouette Coefficient is calculated using the mean intra-cluster
+    The DISCO score is calculated using the mean intra-cluster
     distance (``a``) and the mean nearest-cluster distance (``b``) for each
-    sample.  The Silhouette Coefficient for a sample is ``(b - a) / max(a,
+    sample.  The DISCO score for a sample is ``(b - a) / max(a,
     b)``.
-    Note that Silhouette Coefficient is only defined if number of labels
+    Note that DISCO score is only defined if number of labels
     is 2 ``<= n_labels <= n_samples - 1``.
 
-    This function returns the Silhouette Coefficient for each sample.
+    This function returns the DISCO score for each sample.
 
     The best value is 1 and the worst value is -1. Values near 0 indicate
     overlapping clusters.
@@ -143,7 +113,7 @@ def disco_samples(
     Returns
     -------
     silhouette : array-like of shape (n_samples,)
-        Silhouette Coefficients for each sample.
+        DISCO scores for each sample.
 
     References
     ----------
@@ -153,7 +123,7 @@ def disco_samples(
        and Applied Mathematics 20: 53-65.
        <https://www.sciencedirect.com/science/article/pii/0377042787901257>`_
 
-    .. [2] `Wikipedia entry on the Silhouette Coefficient
+    .. [2] `Wikipedia entry on the DISCO score
        <https://en.wikipedia.org/wiki/Silhouette_(clustering)>`_
 
     Examples
@@ -215,18 +185,18 @@ def p_cluster(
     if len(X) == 0:
         return np.array([])
 
+    if len(X) == 1:
+        return np.array([0])
+
+    if len(X) == len(set(labels)):
+        return np.zeros(len(X))
+
     if precomputed_dc_dists:
         if X.ndim != 2 or X.shape[0] != X.shape[1]:
             raise ValueError("`X` needs to be a distance matrix if `precomputed_dc_dists` is `True`.")
         dc_dists = X
     else:
         dc_dists = DCTree(X, min_points=min_points, no_fastindex=False).dc_distances()
-
-    if len(dc_dists) == 1:
-        return np.array([0])
-
-    if len(dc_dists) == len(set(labels)):
-        return np.zeros(len(dc_dists))
 
     return silhouette_samples(dc_dists, labels, metric="precomputed")
 
@@ -301,174 +271,3 @@ def p_noise(
 
     # Noise evaluation is minimum of core and distance property
     return p_sparse, p_far
-
-
-def silhouette_samples(X, labels, *, metric="euclidean", **kwds):
-    """Compute the Silhouette Coefficient for each sample.
-
-    The Silhouette Coefficient is a measure of how well samples are clustered
-    with samples that are similar to themselves. Clustering models with a high
-    Silhouette Coefficient are said to be dense, where samples in the same
-    cluster are similar to each other, and well separated, where samples in
-    different clusters are not very similar to each other.
-
-    The Silhouette Coefficient is calculated using the mean intra-cluster
-    distance (``a``) and the mean nearest-cluster distance (``b``) for each
-    sample.  The Silhouette Coefficient for a sample is ``(b - a) / max(a,
-    b)``.
-    Note that Silhouette Coefficient is only defined if number of labels
-    is 2 ``<= n_labels <= n_samples - 1``.
-
-    This function returns the Silhouette Coefficient for each sample.
-
-    The best value is 1 and the worst value is -1. Values near 0 indicate
-    overlapping clusters.
-
-    Read more in the :ref:`User Guide <silhouette_coefficient>`.
-
-    Parameters
-    ----------
-    X : {array-like, sparse matrix} of shape (n_samples_a, n_samples_a) if metric == \
-            "precomputed" or (n_samples_a, n_features) otherwise
-        An array of pairwise distances between samples, or a feature array. If
-        a sparse matrix is provided, CSR format should be favoured avoiding
-        an additional copy.
-
-    labels : array-like of shape (n_samples,)
-        Label values for each sample.
-
-    metric : str or callable, default='euclidean'
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by :func:`~sklearn.metrics.pairwise_distances`.
-        If ``X`` is the distance array itself, use "precomputed" as the metric.
-        Precomputed distance matrices must have 0 along the diagonal.
-
-    **kwds : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a ``scipy.spatial.distance`` metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-
-    Returns
-    -------
-    silhouette : array-like of shape (n_samples,)
-        Silhouette Coefficients for each sample.
-
-    References
-    ----------
-
-    .. [1] `Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
-       Interpretation and Validation of Cluster Analysis". Computational
-       and Applied Mathematics 20: 53-65.
-       <https://www.sciencedirect.com/science/article/pii/0377042787901257>`_
-
-    .. [2] `Wikipedia entry on the Silhouette Coefficient
-       <https://en.wikipedia.org/wiki/Silhouette_(clustering)>`_
-
-    Examples
-    --------
-    >>> from sklearn.metrics import silhouette_samples
-    >>> from sklearn.datasets import make_blobs
-    >>> from sklearn.cluster import KMeans
-    >>> X, y = make_blobs(n_samples=50, random_state=42)
-    >>> kmeans = KMeans(n_clusters=3, random_state=42)
-    >>> labels = kmeans.fit_predict(X)
-    >>> silhouette_samples(X, labels)
-    array([...])
-    """
-
-    # Check for non-zero diagonal entries in precomputed distance matrix
-    if metric == "precomputed":
-        error_msg = ValueError(
-            "The precomputed distance matrix contains non-zero " "elements on the diagonal. Use np.fill_diagonal(X, 0)."
-        )
-        if X.dtype.kind == "f":
-            atol = np.finfo(X.dtype).eps * 100
-            if np.any(np.abs(X.diagonal()) > atol):
-                raise error_msg
-        elif np.any(X.diagonal() != 0):  # integral dtype
-            raise error_msg
-
-    le = LabelEncoder()
-    labels = le.fit_transform(labels)
-    n_samples = len(labels)
-    label_freqs = np.bincount(labels)
-    check_number_of_labels(len(le.classes_), n_samples)
-
-    kwds["metric"] = metric
-    reduce_func = functools.partial(_silhouette_reduce, labels=labels, label_freqs=label_freqs)
-    results = zip(*pairwise_distances_chunked(X, reduce_func=reduce_func, **kwds))
-    intra_clust_dists, inter_clust_dists = results
-    intra_clust_dists = np.concatenate(intra_clust_dists)
-    inter_clust_dists = np.concatenate(inter_clust_dists)
-
-    denom = (label_freqs - 1).take(labels, mode="clip")
-    with np.errstate(divide="ignore", invalid="ignore"):
-        intra_clust_dists /= denom
-
-    sil_samples = inter_clust_dists - intra_clust_dists
-    with np.errstate(divide="ignore", invalid="ignore"):
-        sil_samples /= np.maximum(intra_clust_dists, inter_clust_dists)
-    # nan values are for clusters of size 1, and should be 0
-    return np.nan_to_num(sil_samples)
-
-
-def check_number_of_labels(n_labels, n_samples):
-    """Check that number of labels are valid.
-
-    Parameters
-    ----------
-    n_labels : int
-        Number of labels.
-
-    n_samples : int
-        Number of samples.
-    """
-    if not 1 < n_labels <= n_samples:
-        raise ValueError("Number of labels is %d. Valid values are 2 to n_samples - 1 (inclusive)" % n_labels)
-
-
-def _silhouette_reduce(D_chunk, start, labels, label_freqs):
-    """Accumulate silhouette statistics for vertical chunk of X.
-
-    Parameters
-    ----------
-    D_chunk : {array-like, sparse matrix} of shape (n_chunk_samples, n_samples)
-        Precomputed distances for a chunk. If a sparse matrix is provided,
-        only CSR format is accepted.
-    start : int
-        First index in the chunk.
-    labels : array-like of shape (n_samples,)
-        Corresponding cluster labels, encoded as {0, ..., n_clusters-1}.
-    label_freqs : array-like
-        Distribution of cluster labels in ``labels``.
-    """
-    n_chunk_samples = D_chunk.shape[0]
-    # accumulate distances from each sample to each cluster
-    cluster_distances = np.zeros((n_chunk_samples, len(label_freqs)), dtype=D_chunk.dtype)
-
-    if issparse(D_chunk):
-        if D_chunk.format != "csr":
-            raise TypeError("Expected CSR matrix. Please pass sparse matrix in CSR format.")
-        for i in range(n_chunk_samples):
-            indptr = D_chunk.indptr
-            indices = D_chunk.indices[indptr[i] : indptr[i + 1]]
-            sample_weights = D_chunk.data[indptr[i] : indptr[i + 1]]
-            sample_labels = np.take(labels, indices)
-            cluster_distances[i] += np.bincount(sample_labels, weights=sample_weights, minlength=len(label_freqs))
-    else:
-        for i in range(n_chunk_samples):
-            sample_weights = D_chunk[i]
-            sample_labels = labels
-            cluster_distances[i] += np.bincount(sample_labels, weights=sample_weights, minlength=len(label_freqs))
-
-    # intra_index selects intra-cluster distances within cluster_distances
-    end = start + n_chunk_samples
-    intra_index = (np.arange(n_chunk_samples), labels[start:end])
-    # intra_cluster_distances are averaged over cluster size outside this function
-    intra_cluster_distances = cluster_distances[intra_index]
-    # of the remaining distances we normalise and extract the minimum
-    cluster_distances[intra_index] = np.inf
-    cluster_distances /= label_freqs
-    inter_cluster_distances = cluster_distances.min(axis=1)
-    return intra_cluster_distances, inter_cluster_distances
