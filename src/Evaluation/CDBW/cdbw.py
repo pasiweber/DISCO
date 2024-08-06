2fda4b0831641922fe8f0bffef8642a1ccb3866f
l.krieger <l.krieger@fz-juelich.de>
2024-08-06 13:15:41 +0200

    update template for implementation details

--- src/Evaluation/CDBW/cdbw.py
+++ src/Evaluation/CDBW/cdbw.py
@@ -9,13 +9,13 @@ from scipy.spatial import ConvexHull
 from scipy.spatial.distance import cdist
 
 
-# Implementation of CDBW from Alexander Lashkov (alashkov83)
-# Source: https://github.com/alashkov83/CDbw
-# License: MIT License
-# Paper: Halkidi, Maria, and Michalis Vazirgiannis.
-#        "A density-based cluster validity approach using multi-representatives."
-#        Pattern Recognition Letters 29.6 (2008): 773-786.
-# Modifications:
+# Implementation of CDbw by
+# - Author: Alexander Lashkov - Github user `alashkov83`
+# - Source: https://github.com/alashkov83/CDbw
+# - License: MIT License
+# - Paper: https://doi.org/10.1016/j.patrec.2007.12.011
+# - Paper-Authors: Halkidi, Maria, and Michalis Vazirgiannis.
+# - Our modifications:
 #    (1) add option QJ to avoid missing dimensions
 #    (2) introduction of dict to avoid double computation of convex hull (line 210)
 #    (3) 591-600 to move labeling from to zero, e.g., 1,3,5 -> 0,1,2
